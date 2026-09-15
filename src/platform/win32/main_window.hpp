4d5ac5c8f840360f76aa838643f4b5d450012790
tom <261801702+techrote@users.noreply.github.com>
2026-09-16 00:51:59 +0100

    FM-001 expose smoke lifecycle flag

--- src/platform/win32/main_window.hpp
+++ src/platform/win32/main_window.hpp
@@ -4,6 +4,6 @@
 
 namespace faultmine::platform::win32 {
 
-int run_application(HINSTANCE instance, int show_command);
+int run_application(HINSTANCE instance, int show_command, bool smoke_test);
 
 }  // namespace faultmine::platform::win32
