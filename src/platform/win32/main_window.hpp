933bcfcac12490bb24c18cbd3fc454d644dab8a1
tom <261801702+techrote@users.noreply.github.com>
2026-09-16 00:45:47 +0100

    FM-001 add Win32 window boundary

---
+++ src/platform/win32/main_window.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <windows.h>
+
+namespace faultmine::platform::win32 {
+
+int run_application(HINSTANCE instance, int show_command);
+
+}  // namespace faultmine::platform::win32
