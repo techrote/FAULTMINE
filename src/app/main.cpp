143a5cf39281f146818b1afa5dcbe344a56040bd
tom <261801702+techrote@users.noreply.github.com>
2026-09-16 00:51:54 +0100

    FM-001 add window lifecycle smoke mode

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -2,6 +2,16 @@
 
 #include <windows.h>
 
-int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int show_command) {
-    return faultmine::platform::win32::run_application(instance, show_command);
+#include <string_view>
+
+int WINAPI wWinMain(
+    HINSTANCE instance,
+    HINSTANCE,
+    PWSTR command_line,
+    int show_command) {
+    const std::wstring_view arguments =
+        command_line != nullptr ? std::wstring_view{command_line} : std::wstring_view{};
+    const bool smoke_test = arguments == L"--smoke-test";
+
+    return faultmine::platform::win32::run_application(instance, show_command, smoke_test);
 }
