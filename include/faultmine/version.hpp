6668a7e049058085a560cf8919194824786f3dcf
tom <261801702+techrote@users.noreply.github.com>
2026-09-17 17:06:28 +0100

    FM-015: establish v1 application version contract

---
+++ include/faultmine/version.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string_view>
+
+namespace faultmine {
+
+inline constexpr std::string_view kApplicationVersion = "1.0.0";
+inline constexpr std::string_view kReleaseChannel = "v1";
+inline constexpr std::string_view kSupportedPlatform = "Windows x64";
+
+}  // namespace faultmine
