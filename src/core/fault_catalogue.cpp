b8b4b3ebd6319b1a40d1bb975622954c83fa6a49
tom <261801702+techrote@users.noreply.github.com>
2026-09-17 10:41:38 +0100

    FM-011 compose timeline-rate metadata into default registry

--- src/core/fault_catalogue.cpp
+++ src/core/fault_catalogue.cpp
@@ -5,6 +5,7 @@
 #include "faultmine/representation_bit_operators.hpp"
 #include "faultmine/starter_operators.hpp"
 #include "faultmine/temporal.hpp"
+#include "faultmine/temporal_timeline.hpp"
 
 #include <cstdint>
 #include <stdexcept>
@@ -110,6 +111,7 @@ FaultRegistry make_default_fault_registry() {
     register_memory_addressing_faults(registry);
     register_representation_bit_faults(registry);
     register_colour_faults(registry);
+    register_temporal_timeline_fault(registry);
     register_temporal_faults(registry);
     return registry;
 }
